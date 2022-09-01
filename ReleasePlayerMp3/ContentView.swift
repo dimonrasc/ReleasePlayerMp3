8f555370800fa32b5f45e8ad2c703730db02198a
hu_f <hu_f@live.com>
2022-09-01 21:41:35 +0300

    Init

--- ReleasePlayerMp3/ContentView.swift
+++ ReleasePlayerMp3/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var speed = 50.0
+    @State private var isEditing = false
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+            Spacer()
+            Text("Title Albom")
+                .font(.system(size: 30, weight: .light, design: .serif))
+                .italic()
+                .lineLimit(1)
+            Spacer()
+            Image("cyrcleImageAlbom")
+                .frame(width: 200, height: 200)
+                .background(Color.blue)
+            Spacer()
+            Slider(value: $speed,
+                   in: 0...100,
+                   step: 5){
+                Text("Speed")
+            } minimumValueLabel: {
+                Text("0")
+            } maximumValueLabel: {
+                Text("100")
+            } onEditingChanged: { editing in
+                isEditing = editing
+            }
+            Text("\(speed)")
+                .foregroundColor(isEditing ? .red : .blue)
+            Spacer()
+            HStack{
+                Button{
+                    print("pressed play/pause")
+                } label: {
+                    Text("Play/Pause")
+                }
+                Spacer()
+                Button{
+                    print("pressed next track")
+                } label: {
+                    Text("Next track")
+                }
+                Spacer()
+                Button{
+                    print("pressed folders")
+                } label: {
+                    Text("Folders")
+                }
+            }
             .padding()
+        }
+        .padding()
     }
 }
 
