298123d6c6f3858020570718067353741b06e1e8
hu_f <hu_f@live.com>
2022-09-01 20:47:10 +0300

    Initial Commit

---
+++ ReleasePlayerMp3/ReleasePlayerMp3App.swift
@@ -0,0 +1,17 @@
+//
+//  ReleasePlayerMp3App.swift
+//  ReleasePlayerMp3
+//
+//  Created by huf on 01.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReleasePlayerMp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
